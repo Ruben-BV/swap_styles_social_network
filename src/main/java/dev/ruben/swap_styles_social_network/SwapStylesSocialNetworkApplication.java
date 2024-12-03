16d365ca38b54120eec204c7ce166af8294faf54
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-03 01:38:20 +0100

    Proyecto Springboot inicializado

---
+++ src/main/java/dev/ruben/swap_styles_social_network/SwapStylesSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package dev.ruben.swap_styles_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapStylesSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapStylesSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ruben/swap_styles_social_network/SwapStylesSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruben.swap_styles_social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapStylesSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
