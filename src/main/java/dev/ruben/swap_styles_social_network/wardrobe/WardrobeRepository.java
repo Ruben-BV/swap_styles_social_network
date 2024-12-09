6697abdedc1111cc11f3feef9548ee82693feca5
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-09 03:40:29 +0100

    Wardrobe Repository Implemented

---
+++ src/main/java/dev/ruben/swap_styles_social_network/wardrobe/WardrobeRepository.java
@@ -0,0 +1,8 @@
+package dev.ruben.swap_styles_social_network.wardrobe;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WardrobeRepository extends JpaRepository <Wardrobe, Long> {
+    
+}
