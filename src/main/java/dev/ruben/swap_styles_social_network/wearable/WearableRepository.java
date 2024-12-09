0897ea6693f0491270aa490c7544427e3d510beb
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-09 23:07:20 +0100

    Wearable Repository Implemented

---
+++ src/main/java/dev/ruben/swap_styles_social_network/wearable/WearableRepository.java
@@ -0,0 +1,7 @@
+package dev.ruben.swap_styles_social_network.wearable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WearableRepository extends JpaRepository<Wearable, Long> {
+    
+}
