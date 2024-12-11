7f65194fa5498cc5edabdf19d58e6a61589fbfb5
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-11 12:10:37 +0100

    Tests Wardrobe Service

---
+++ src/test/java/dev/ruben/swap_styles_social_network/wardrobe/WardrobeServiceTest.java
@@ -0,0 +1,123 @@
+package dev.ruben.swap_styles_social_network.wardrobe;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.ruben.swap_styles_social_network.user.User;
+import dev.ruben.swap_styles_social_network.user.UserRepository;
+import dev.ruben.swap_styles_social_network.user.UserService;
+import dev.ruben.swap_styles_social_network.wearable.Wearable;
+
+@ExtendWith(MockitoExtension.class)
+public class WardrobeServiceTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private WardrobeRepository wardrobeRepository;
+    
+    @InjectMocks
+    WardrobeService wardrobeService;
+    @Mock
+    private Wardrobe mockWardrobe;
+    private WardrobeDTO wardrobeDTO;
+    private User user;
+    
+    @BeforeEach
+    void setUp() {
+        mockWardrobe = new Wardrobe(1L, new User(2L, "USER", "Test User", "testuser@email.com", "Profile Image"), "My Wardrobe", new ArrayList<Wearable>());
+        wardrobeDTO = new WardrobeDTO();
+            wardrobeDTO.setUserId(2L);
+            wardrobeDTO.setWardrobeId(1L);
+            wardrobeDTO.setWardrobeName("Test Wardrobe");
+        user = new User(2L, "USER", "Test User", "testuser@email.com", "Profile Image");
+    
+    }
+    @Test
+    public void testCreateWardrobeUserFound() {
+
+        when(userRepository.findById(2L)).thenReturn(java.util.Optional.of(user));
+        when(wardrobeRepository.save(any(Wardrobe.class))).thenReturn(mockWardrobe);
+
+        WardrobeDTO result = wardrobeService.createWardrobe(wardrobeDTO);
+
+        assertNotNull(result);
+
+        assertEquals(mockWardrobe.getWardrobeId(), result.getWardrobeId());
+        assertEquals(mockWardrobe.getWardrobeName(), result.getWardrobeName());
+
+    }
+
+    @Test
+    public void testCreateWardrobe_UserNotFound() {
+
+        when(userRepository.findById(2L)).thenReturn(java.util.Optional.empty());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            wardrobeService.createWardrobe(wardrobeDTO);
+        });
+
+        assertEquals("User not found with userId: 2. Status: 404 NOT_FOUND", exception.getMessage());
+
+    }
+
+    @Test
+    public void testGetAllWardrobes() {
+        Wardrobe mockWardrobe = new Wardrobe(1L, new User(), "Test Wardrobe", new ArrayList<>());
+        
+        List<Wardrobe> mockWardrobes = new ArrayList<>();
+        mockWardrobes.add(mockWardrobe);
+        
+        when(wardrobeRepository.findAll()).thenReturn(mockWardrobes);
+        
+        List<WardrobeDTO> result = wardrobeService.getAllWardrobes();
+        
+        WardrobeDTO expectedDTO = new WardrobeDTO(mockWardrobe);
+        
+        assertEquals(1, result.size());
+    }
+
+
+    @Test
+    public void testGetWardrobesByUserId() {
+        User user = new User(2L, "USER", "Test User", "testuser@email.com", "Profile Image");
+        
+        Wardrobe mockWardrobe = new Wardrobe(1L, user, "Test Wardrobe", new ArrayList<>());
+        
+        List<Wardrobe> mockWardrobes = new ArrayList<>();
+        mockWardrobes.add(mockWardrobe);
+        
+        when(wardrobeRepository.findAll()).thenReturn(mockWardrobes);
+    
+        WardrobeDTO expectedDTO = new WardrobeDTO(mockWardrobe);
+    
+        List<WardrobeDTO> result = wardrobeService.getWardrobesByUserId(2L);
+    
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    public void testGetWardrobeById() {
+        User user = new User(2L, "USER", "Test User", "testuser@email.com", "Profile Image");
+        Wardrobe mockWardrobe = new Wardrobe(1L, user, "Test Wardrobe", new ArrayList<>());
+        WardrobeDTO expectedDTO = new WardrobeDTO(mockWardrobe);
+
+        when(wardrobeRepository.findById(1L)).thenReturn(Optional.of(mockWardrobe));
+
+        Optional<WardrobeDTO> result = wardrobeService.getWardrobeById(1L);
+
+        assertTrue(result.isPresent());
+    }
+
+}
