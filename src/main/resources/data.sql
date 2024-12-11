4d25c7b554c79a222ff71deb943d8e815a1d2c7f
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-11 13:08:03 +0100

    data.sql: Data were added to friendships table. Profile images and wearables images were added.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,7 @@ INSERT INTO wardrobes (wardrobe_id, user_id, wardrobe_name) VALUES (default, 4, 
 INSERT INTO wearables (wearable_id, wardrobe_id, wearable_type, wearable_name, wearable_description, visibility, photos_list) VALUES (default, 3, 6, 'My skirt', 'Black long skirt', true, '');
 INSERT INTO wearables (wearable_id, wardrobe_id, wearable_type, wearable_name, wearable_description, visibility, photos_list) VALUES (default, 4, 9, 'Harry Potter', 'With plastic glasses, cloak and magic wand', false, '');
 INSERT INTO wearables (wearable_id, wardrobe_id, wearable_type, wearable_name, wearable_description, visibility, photos_list) VALUES (default, 4, 9, 'Superman', 'Bodysuit and cloak', true, '');
+
+INSERT INTO friendships (friendship_id, user1_id, user2_id, friendship_status) VALUES (default, 3, 4, 1);
+INSERT INTO friendships (friendship_id, user1_id, user2_id, friendship_status) VALUES (default, 3, 5, 0);
+INSERT INTO friendships (friendship_id, user1_id, user2_id, friendship_status) VALUES (default, 2, 5, 2);
